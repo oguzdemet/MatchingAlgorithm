f5701a0d47944e2867cb357ac9537196c165a918
Oğuz Kaan DEMET <demetoguzkaan@gmail.com>
2023-07-06 17:05:38 +0300

    Add project files.

---
+++ Algorithm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatchingAlgorithm
+{
+    internal class Algorithm
+    {
+        public static int[][][] BuildTournamentSchedule(int numberOfTeams)
+        {
+            int[][][] scheduleArray;
+
+            for (int i = 0; i< numberOfTeams; i++)
+            {
+
+            }
+
+
+            return scheduleArray;
+        }
+    }
+}
